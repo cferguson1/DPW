1ce45b2809bab88f125acf445cb4fd54fb200db0
collin <collin@corephp.com>
2014-06-05 22:06:27 -0400

    get inputs in dict and array

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-name = raw_input("Pick a name: ")
-animal = raw_input("Pick an animal ")
-verb = raw_input("Pick a verb: ")
+#Get names in array
+character = []
+name = character.append( raw_input("Pick a name: ") )
+animal = character.append( raw_input("Pick an animal ") )
+verb = character.append( raw_input("Pick a verb: ") )
+
 number_one = raw_input("Pick a number (1-10)" )
 number_two = raw_input("Pick another number (1-10)" )
 number_three = raw_input("Pick one last number (1-10)" )
+numbers = dict()
+numbers = {"Age":number_one, "Friends":number_two, "Height":number_three}
 
-madlib = "One day, there was an ugly {animal} named {name}. "
+madlib = "One day there was a {character[1]} named {character[0]}. {character[0]} "
+
+
