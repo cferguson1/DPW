7084f363f6cfb2245c22c6b1e89d2a58a7e4713c
collin <collin@corephp.com>
2014-06-05 21:38:01 -0400

    collect inputs

---
+++ main.py
@@ -0,0 +1,8 @@
+name = raw_input("Pick a name: ")
+animal = raw_input("Pick an animal ")
+verb = raw_input("Pick a verb: ")
+number_one = raw_input("Pick a number (1-10)" )
+number_two = raw_input("Pick another number (1-10)" )
+number_three = raw_input("Pick one last number (1-10)" )
+
+madlib = "One day, there was an ugly {animal} named {name}. "
