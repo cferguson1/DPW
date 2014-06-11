b288d97da7b33e2b6b73189757fb3e665a05c0e5
collin <collin@corephp.com>
2014-06-11 19:35:12 -0400

    review projects

---
+++ classes-html/main.py
@@ -0,0 +1,14 @@
+
+import webapp2
+from pages import Page #from package, import Class
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        p = Page()
+        p.body = "Hello World"
+        self.response.write(p.print_out())
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
