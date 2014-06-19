9bcb213523257c9a987c3e92c919b07f157b9397
collin <collin@corephp.com>
2014-06-19 18:21:56 -0400

    setting up AbstractAnimal Class

--- fox/main.py
+++ fox/main.py
@@ -5,6 +5,29 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
+class AbstractAnimal(object):
+    #initialize Object
+    def __init__(self):
+        self._open = '''
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>What does the fox say?</title>
+        <link rel="stylesheet" type="text/css" href="css/styles.css"/>
+    </head>
+    <body>
+        '''
+        self.body = ""
+        self._close = '''
+    </body>
+</html>
+        '''
+
+    #print content
+    def print_out(self):
+        pass
+
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
