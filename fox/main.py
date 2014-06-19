cb70527815a8fafe320a57bdcd92e490ac582a50
collin <collin@corephp.com>
2014-06-19 19:45:20 -0400

    override superclass function / pass array in

--- fox/main.py
+++ fox/main.py
@@ -5,6 +5,8 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         #create instance of Animal
         a = Animal()
+        #pass some animals
+        a.inputs = ['Name','Phylum','Class','Order','Family','Genus','Species','imgurl','lifespan','Habitat','Geolocation' ]
         self.response.write(a.print_out())
 
 class AbstractAnimal(object):
@@ -49,26 +51,31 @@ class Animal(AbstractAnimal):
     def inputs(self, arr):
         #set it to the array we passed in
         self.__inputs = arr
+        print arr
         for item in arr:
             self._figure_inputs += '''
 <figure>
     <figcaption>
             '''
-            self._figure_inputs += '<p><span class="label">Phylum: </span>' + item['phylum'] + '</p>'
-            self._figure_inputs += '<p><span class="label">Class: </span>' + item['class'] + '</p>'
-            self._figure_inputs += '<p><span class="label">Order: </span>' + item['order'] + '</p>'
-            self._figure_inputs += '<p><span class="label">Family: </span>' + item['family'] + '</p>'
-            self._figure_inputs += '<p><span class="label">Genus: </span>' + item['genus'] + '</p>'
-            self._figure_inputs += '<p><span class="label">URL: </span>' + item['imgurl'] + '</p>'
-            self._figure_inputs += '<p><span class="label">Average Lifespan: </span>' + item['lifespan'] + '</p>'
-            self._figure_inputs += '<p><span class="label">Habitat: </span>' + item['habitat'] + '</p>'
-            self._figure_inputs += '<p><span class="label">Geolocation: </span>' + item['geolocation'] + '</p>'
+            self._figure_inputs += '<p>' + item[0] + '</p>'
+            self._figure_inputs += '<p><span class="label">Phylum: </span>' + item[1] + '</p>'
+            self._figure_inputs += '<p><span class="label">Class: </span>' + item[2] + '</p>'
+            self._figure_inputs += '<p><span class="label">Order: </span>' + item[3] + '</p>'
+            self._figure_inputs += '<p><span class="label">Family: </span>' + item[4] + '</p>'
+            self._figure_inputs += '<p><span class="label">Genus: </span>' + item[5] + '</p>'
+            self._figure_inputs += '<p><span class="label">Species: </span>' + item[6] + '</p>'
+            self._figure_inputs += '<p><span class="label">URL: </span>' + item[7] + '</p>'
+            self._figure_inputs += '<p><span class="label">Average Lifespan: </span>' + item[8] + '</p>'
+            self._figure_inputs += '<p><span class="label">Habitat: </span>' + item[9] + '</p>'
+            self._figure_inputs += '<p><span class="label">Geolocation: </span>' + item[10] + '</p>'
             self._figure_inputs += '''
     </figcatpion>
 </figure>
             '''
 
-
+    #override superclass and print this
+    def print_out(self):
+        return self._open + self._figure_inputs + self._close
 
 
 
