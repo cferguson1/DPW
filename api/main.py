662d17eb8046e3e684e03d4b66c9a738543ce7c7
collin <collin@corephp.com>
2014-06-26 18:27:32 -0400

    use polymorphism template

--- api/main.py
+++ api/main.py
@@ -1,24 +1,61 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        p = FormPage()
+        p.inputs = [ ['first_name', 'text', 'First Name'],['last_name', 'text', 'Last Name'],['Submit', 'submit'] ]
+        self.response.write(p.print_out())
+
+class Page(object): #borrowing stuff from the object class
+    def __init__(self):
+        self._head = '''
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>'''
+
+        self._body = 'Filler'
+        self._close = '''
+    </body>
+</html>'''
+
+    def print_out(self):
+        return self._head + self._body + self._close
+
+class FormPage(Page):
+    def __init__(self):
+        #constructor func for the super class
+        super(FormPage, self).__init__() #Page.__init__()
+        self._form_open = '<form method="GET">'
+        self._form_close = '</form>'
+        self.__inputs = []
+        self._form_inputs = ''
+
+    @property
+    def inputs(self):
+        pass
+
+    @inputs.setter
+    def inputs(self, arr):
+        #change my private inputs variable
+        self.__inputs = arr
+        #sort through the array and create HTML inputs
+        for item in arr:
+            self._form_inputs += '<input type="' + item[1] + '" name="' + item[0]
+            #if there is a third item, add it in
+            try:
+                self._form_inputs += '" placeholder="' + item[2] + '" />'
+            #otherwise end the tag
+            except:
+                self._form_inputs += '" />'
+
+        print self._form_inputs
+
+    def print_out(self):
+        return self._head + self._body + self._form_open + self._form_inputs + self._form_close + self._close
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
